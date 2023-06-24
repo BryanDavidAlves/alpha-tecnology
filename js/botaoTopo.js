5b5e4dc288c5246d4324763c5f46846f7a636923
Bryan David <bryandavidsalvinoalves@gmail.com>
2023-06-24 20:44:39 -0300

    identando todos os codigos

--- js/botaoTopo.js
+++ js/botaoTopo.js
@@ -1,22 +1,22 @@
-const btn=document.getElementById("btnTop");
-function ocultar(){
-    if(window.scrollY>20){
-        btn.style.display="flex"
-    }else{
-        btn.style.display="none"
+const btn = document.getElementById("btnTop");
+function ocultar() {
+    if (window.scrollY > 20) {
+        btn.style.display = "flex"
+    } else {
+        btn.style.display = "none"
     }
 }
 ocultar()
 
 document.addEventListener('scroll', ocultar)
 
-function clickMenu(){
-    if(itens.style.display == 'block' && bgColorJs.style.display =='block'){
-        itens.style.display ='none'
-        bgColorJs.style.display ='none'
-    }else{
+function clickMenu() {
+    if (itens.style.display == 'block' && bgColorJs.style.display == 'block') {
+        itens.style.display = 'none'
+        bgColorJs.style.display = 'none'
+    } else {
         itens.style.display = 'block'
-        bgColorJs.style.display ='block'
+        bgColorJs.style.display = 'block'
     }
 }
 
